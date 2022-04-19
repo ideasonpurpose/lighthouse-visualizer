19b7a4c2c155ee25a468d2715bc794bb8082c966
Codrin Pavel <zerospree@yahoo.com>
2022-04-19 17:03:57 +0300

    Build and build settings

--- 11ty/config.js
+++ 11ty/config.js
@@ -13,19 +13,7 @@ const isProduction = process.env.ELEVENTY_ENV === 'production';
 module.exports = {
   ISPROD: isProduction,
 
-  // Extranet credentials
-  // user: 'EXTRANET_USER',
-  // pass: 'EXTRANET_PASSWORD', 
-
-  // Internal Stage
-  BASEURL: isProduction ? "/PROJECT_NAME/internalreview" : "",
-  PRODURL: isProduction ? "https://client.ideasonpurpose.com" : "",
-
-  // Client Stage
-  // BASEURL: isProduction ? "/PROJECT_NAME/clientreview" : "",
-  // PRODURL: isProduction ? "https://client.ideasonpurpose.com" : "",
-
-  // Production
-  // BASEURL: isProduction ? "/" : "",
-  // PRODURL: isProduction ? "https://clientdomain.com" : "",
+  // Internal Stage Settings
+  BASEURL: isProduction ? "/lighthouse-visualizer/dist" : "",
+  PRODURL: isProduction ? "https://ideasonpurpose.github.io" : "",
 };
